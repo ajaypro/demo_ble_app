37b375715d2b155045a48bcdb0f7bf93869b458d
Nikhil Kumar Singh <nikhilsingh@technoidentity.com>
2021-03-24 11:47:14 +0530

    .idea Remove

---
+++ VitalzApp/app/src/main/java/com/technoidentity/vitalz/tab/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.technoidentity.vitalz.tab.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.technoidentity.vitalz.R
+
+class LoginActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_login)
+  }
+}
