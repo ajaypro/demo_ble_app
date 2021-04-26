c324b89e3a324c190985a74362a87be067b844f5
Nikhil Kumar Singh <nikhilsingh@technoidentity.com>
2021-04-26 14:39:34 +0530

    Chnaged to TimeCounter

--- VitalzApp/app/src/main/java/com/technoidentity/vitalz/home/SplashFragment.kt
+++ VitalzApp/app/src/main/java/com/technoidentity/vitalz/home/SplashFragment.kt
@@ -1,23 +1,29 @@
 package com.technoidentity.vitalz.home
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.technoidentity.vitalz.R
 
-
 class SplashFragment : Fragment(R.layout.fragment_splash_screen) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController: NavController = Navigation.findNavController(view)
-        val handler = Handler()
-        val runnable = Runnable { //Second fragment after 5 seconds appears
-            navController.navigate(R.id.action_splashFragment2_to_userSelectionFragment2)
+        val countDownTimer = object : CountDownTimer(3000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+            }
+
+            override fun onFinish() {
+                navController.navigate(R.id.action_splashFragment2_to_userSelectionFragment2)
+            }
+
         }
-        handler.postDelayed(runnable, 2000)
+        countDownTimer.start()
     }
 }
