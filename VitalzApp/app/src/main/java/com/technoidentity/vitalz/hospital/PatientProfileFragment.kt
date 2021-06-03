beeb348d292c5de5e4524ca7e578f1af02d32497
Nikhil Kumar Singh <nikhilsingh@technoidentity.com>
2021-06-03 16:27:22 +0530

    Profile Page new Ui

--- VitalzApp/app/src/main/java/com/technoidentity/vitalz/hospital/PatientProfileFragment.kt
+++ VitalzApp/app/src/main/java/com/technoidentity/vitalz/hospital/PatientProfileFragment.kt
@@ -1,7 +1,33 @@
 package com.technoidentity.vitalz.hospital
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.technoidentity.vitalz.R
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.technoidentity.vitalz.databinding.FragmentViewProfileBinding
 
-class PatientProfileFragment : Fragment(R.layout.fragment_patient_profile) {
+class PatientProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentViewProfileBinding
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentViewProfileBinding.inflate(layoutInflater)
+        navController =  Navigation.findNavController(container!!)
+
+        binding.ivBackBtn.setOnClickListener {
+            navController.navigateUp()
+        }
+
+
+
+        return binding.root
+    }
 }
