fbddd2451c0c3a7b891da5fd0c4846c2e15a4275
Nikhil Kumar Singh <nikhilsingh@technoidentity.com>
2021-06-09 13:53:45 +0530

    Heart Line Graph Implemented

--- VitalzApp/app/src/main/java/com/technoidentity/vitalz/dashboard/NurseCareTakerDashboardFragment.kt
+++ VitalzApp/app/src/main/java/com/technoidentity/vitalz/dashboard/NurseCareTakerDashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.technoidentity.vitalz.dashboard
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -10,14 +11,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.listener.OnChartGestureListener
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.technoidentity.vitalz.R
-import com.technoidentity.vitalz.data.datamodel.single_patient.HeartRate
 import com.technoidentity.vitalz.data.datamodel.single_patient.SinglePatientDashboardResponse
 import com.technoidentity.vitalz.databinding.CaretakerNurseDashboardBinding
 import com.technoidentity.vitalz.utils.CustomProgressDialog
@@ -31,10 +28,7 @@ class NurseCareTakerDashboardFragment : Fragment() {
     lateinit var binding: CaretakerNurseDashboardBinding
     var navController: NavController? = null
     private lateinit var progressDialog: CustomProgressDialog
-    lateinit var lineDataSet: LineDataSet
-    lateinit var lineListHeart: ArrayList<HeartRate>
     lateinit var singlePatientDashboardResponse: SinglePatientDashboardResponse
-    lateinit var lineData: LineData
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -50,7 +44,6 @@ class NurseCareTakerDashboardFragment : Fragment() {
 
         //Getting Arguments From last Fragment
         patientId = arguments?.getString("patientId")
-        Log.v("Check","Patient Id receiving $patientId")
 
         //Api call to fetch Latest data
         if (patientId != null){
@@ -70,7 +63,41 @@ class NurseCareTakerDashboardFragment : Fragment() {
         }
 
         //pieChart
-        lineListHeart = ArrayList()
+        with(binding.pieChartHeartLayout) {
+            // (1)
+            axisLeft.isEnabled = false
+            axisRight.isEnabled = false
+            xAxis.isEnabled = false
+            legend.isEnabled = false
+            description.isEnabled = false
+
+            // (2)
+            setTouchEnabled(true)
+            isDragEnabled = true
+            setScaleEnabled(false)
+            setPinchZoom(false)
+        }
+
+        val entries: List<Entry> =
+            singlePatientDashboardResponse.heartRate.ratePerMinute.mapIndexed { index, i ->
+                Entry(
+                    index.toFloat(),
+                    i.toFloat()
+                )
+            }
+
+        val dataSet = LineDataSet(entries, "Unused label").apply {
+            color = Color.BLACK
+            valueTextColor = Color.GRAY
+            highLightColor = Color.RED
+            setDrawValues(false)
+            lineWidth = 1.5f
+            isHighlightEnabled = true
+            setDrawHighlightIndicators(false)
+        }
+
+        binding.pieChartHeartLayout.data =  LineData(dataSet)
+        binding.pieChartHeartLayout.invalidate()
 
         return binding.root
     }
