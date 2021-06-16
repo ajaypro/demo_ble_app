4aca13ea23ea7ea2f66a9d17f3f26bd3d3f09105
Nikhil Kumar Singh <nikhilsingh@technoidentity.com>
2021-06-16 23:41:00 +0530

    Changes in Model and Design

--- VitalzApp/app/src/main/java/com/technoidentity/vitalz/hospital/PatientProfileFragment.kt
+++ VitalzApp/app/src/main/java/com/technoidentity/vitalz/hospital/PatientProfileFragment.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavArgs
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.technoidentity.vitalz.data.datamodel.single_patient.SinglePatientDashboardResponse
 import com.technoidentity.vitalz.databinding.FragmentViewProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PatientProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentViewProfileBinding
@@ -22,7 +26,7 @@ class PatientProfileFragment : Fragment() {
 
         //Getting Arguments From last Fragment
         val profileData: SinglePatientDashboardResponse? = arguments?.getParcelable("patientData")
-        setDataInUI(profileData)
+        profileData?.let { setDataInUI(it) }
 
         binding.ivBackBtn.setOnClickListener {
             findNavController().navigateUp()
@@ -31,17 +35,17 @@ class PatientProfileFragment : Fragment() {
         return binding.root
     }
 
-    private fun setDataInUI(profileData: SinglePatientDashboardResponse?) {
-        binding.etName.text = profileData?.name
-        binding.etAge.text = profileData?.age.toString()
-        binding.etGender.text = profileData?.gender
-        binding.etHeight.text = profileData?.height.toString()
-        binding.etWeight.text = profileData?.weight.toString()
-        binding.etAddress.text = profileData?.address
-        binding.tvContactNumber.text = profileData?.doctorContactNumber
-        binding.etEmergencyContactName.text = profileData?.emergencyContactName
-        binding.etEmergencyContactNumber.text = profileData?.emergencyContactNumber
-        binding.etAttendingDoctor.text = profileData?.doctorName
-        binding.etIdHospital.text = profileData?.hospitalId
+    private fun setDataInUI(profileData: SinglePatientDashboardResponse) {
+        binding.etName.text = profileData.name
+        binding.etAge.text = profileData.age.toString()
+        binding.etGender.text = profileData.gender
+        binding.etHeight.text = profileData.height.toString()
+        binding.etWeight.text = profileData.weight.weight.toString()
+        binding.etAddress.text = profileData.address
+        binding.tvContactNumber.text = profileData.doctorContactNumber
+        binding.etEmergencyContactName.text = profileData.emergencyContactName
+        binding.etEmergencyContactNumber.text = profileData.emergencyContactNumber
+        binding.etAttendingDoctor.text = profileData.doctorName
+        binding.etIdHospital.text = profileData.hospitalId
     }
 }
